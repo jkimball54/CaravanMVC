c5b71dc190ed730c22f2347fcc82654c38c48716
jkimball54 <jeremyck54@gmail.com>
2023-08-21 13:16:58 -0600

    forgot ids

--- CaravanMVC/Models/Wagon.cs
+++ CaravanMVC/Models/Wagon.cs
@@ -1,7 +1,10 @@
-﻿namespace CaravanMVC.Models
+﻿using Microsoft.AspNetCore.Mvc.RazorPages.Infrastructure;
+
+namespace CaravanMVC.Models
 {
     public class Wagon
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public int NumWheels { get; set; }
         public bool Covered { get; set; }

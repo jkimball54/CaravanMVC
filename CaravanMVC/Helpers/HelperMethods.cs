9a345d03b8de4d87052271fec4c487ee9fc67fae
jkimball54 <jeremyck54@gmail.com>
2023-08-21 15:22:34 -0600

    Stories 5&6

---
+++ CaravanMVC/Helpers/HelperMethods.cs
@@ -0,0 +1,21 @@
+﻿using CaravanMVC.DataAccess;
+using CaravanMVC.Models;
+
+namespace CaravanMVC.Helpers
+{
+    public static class HelperMethods
+    {
+
+
+        public static List<string> DistinctLocations(CaravanMvcContext context)
+        {
+            return context.Passengers.Select(p => p.Destination).Distinct().ToList();
+        }
+
+        public static int AveragePassengerAge(CaravanMvcContext context)
+        {
+            return Convert.ToInt32(context.Passengers.Average(a => a.Age));
+        }
+    }
+
+}

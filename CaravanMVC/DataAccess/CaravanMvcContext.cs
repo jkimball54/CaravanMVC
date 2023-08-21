c05cc5d119a4cbac2c1cf30699ae273d1bb14069
jkimball54 <jeremyck54@gmail.com>
2023-08-21 13:52:02 -0600

    finished adding wagon index, removed data seeder

--- CaravanMVC/DataAccess/CaravanMvcContext.cs
+++ CaravanMVC/DataAccess/CaravanMvcContext.cs
@@ -14,19 +14,5 @@ namespace CaravanMVC.DataAccess
 
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Wagon>().HasData(
-                new Wagon { Id = 1, Name = "Old Faithful", NumWheels = 4, Covered = true},
-                new Wagon { Id = 2, Name = "Pain-Train", NumWheels = 3, Covered = false}
-            );
-
-            modelBuilder.Entity<Passenger>().HasData(
-                new Passenger { Id = 1, Name = "John", Age = 20, Destination = "Utah"},
-                new Passenger { Id = 2, Name = "Jesse", Age = 25, Destination = "Wisconsin" },
-                new Passenger { Id = 3, Name = "Sarah", Age = 20, Destination = "California" }
-            );
-        }
-
     }
 }
